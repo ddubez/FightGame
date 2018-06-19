1a307529841e67f88a95edeb7b38a41e8e4cef10
ddubez <ddubez40@gmail.com>
2018-06-19 17:57:51 +0200

    Initial Commit

---
+++ FightGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FightGame
+//
+//  Created by David DUBEZ on 19/06/2018.
+//  Copyright © 2018 David DUBEZ. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

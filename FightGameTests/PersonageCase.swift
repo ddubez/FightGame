02016ea6c9a07fc4f34a54f45721ba6b66f0760b
ddubez <ddubez40@gmail.com>
2018-07-03 14:41:28 +0200

    created WeaponCase Test

---
+++ FightGameTests/PersonageCase.swift
@@ -0,0 +1,25 @@
+//
+//  PersonageCase.swift
+//  FightGameTests
+//
+//  Created by David DUBEZ on 03/07/2018.
+//  Copyright © 2018 David DUBEZ. All rights reserved.
+//
+
+import XCTest
+@testable import FightGame
+
+class PersonageCase: XCTestCase {
+
+	// test of creation dagger weapon
+	func testGivenPersonage1has100lifePoints_WhenGettingAttackbyPersonage2withAx_ThenPersonage1isdied() {
+		let personage1 = Combatant(name: "luc")
+		personage1.lifePoints = 100
+		let personage2 = Dwarf(name: "Steeve")
+
+		personage1.isAttackedBy(personage2)
+
+		XCTAssert(personage1.isdead)
+	}
+
+}
